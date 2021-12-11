6b16fd6d2fee90788e12ba54e46a41d485c5368b
Denis.Popov <dpopov.pro@yandex.ru>
2021-12-11 13:53:59 +0300

    git rebase 2

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -3,8 +3,10 @@
 
 count=1
 for param in "$@"; do
-    	echo "\$@ Parameter #$count = $param"
-    	count=$(( $count + 1 ))
+<<<<<<< HEAD
+    echo "Next parameter: $param"
+    count=$(( $count + 1 ))
+>>>>>>> cbb0def... git rebase 2
 done
 
 echo "====="
